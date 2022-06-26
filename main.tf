bffba6c6fac7bd60c60e7028bb0c1505a90f328b
gTech-cloud <aws.elearner@gmail.com>
2022-06-27 01:00:55 +0530

    container group added

--- main.tf
+++ main.tf
@@ -1,12 +1,47 @@
-provider "azurerm" {
-    version = "2.5.0"
-    features {
-      
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.1"
     }
+  }
+}
 
+# 2. Configure the AzureRM Provider
+provider "azurerm" {
+  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
+  # and a Service Principal. More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
+
+  # The features block allows changing the behaviour of the Azure Provider, more
+  # information can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
+  features {}
 }
 
 resource "azurerm_resource_group" "tf_test" {
   name="tfnamerg"
   location="Australia East"
 }
+
+resource "azurerm_container_group" "tfcg_test" {
+  name = "weatherapi"
+  location = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  ip_address_type = "Public"
+  dns_name_label = "gtechcloud-webapp-v1"
+  os_type = "Linux"
+
+  container {
+    name = "weatherapi"
+    image = "gtechcloud/webapp:v1"
+    cpu = "1"
+    memory = "1"
+    ports {
+      port= 80
+      protocol = "TCP"
+    }
+  }
+}
+
